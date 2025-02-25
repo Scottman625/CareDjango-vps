bb0620c31f440e12e94499d9782170c0699947e7
Scottman625 <scottman608@gmail.com>
2025-02-25 10:49:24 +0800

    update app/urls.py

--- app/app/urls.py
+++ app/app/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,6 +14,7 @@ urlpatterns = [
     path('backboard/', include('backboard.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('messageApp/', include('messageApp.urls')),
+    path('', lambda request: redirect('web:index'), name='home'),
 ]
 
 if settings.DEBUG:
