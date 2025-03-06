5fe240c8e2e895a53e984fc8bd022e8b8604a588
Scottman625 <scottman608@gmail.com>
2025-03-06 14:19:23 +0800

    add allow host

--- app/app/settings.py
+++ app/app/settings.py
@@ -16,6 +16,7 @@ from firebase_admin import initialize_app
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -34,7 +35,7 @@ ALLOWED_HOSTS = [
     '149.28.22.217',
     '0.0.0.0',
     'care168.stockfunction.cloud',
-    '*',
+    'www.care168.stockfunction.cloud',
 ]
 
 ADMINS = (
