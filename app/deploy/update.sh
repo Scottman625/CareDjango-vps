eeabaea0f1c79d5ffa654c1636f6f0d1d2e33811
Scottman625 <scottman608@gmail.com>
2025-03-06 14:55:02 +0800

    update deploy file

--- app/deploy/update.sh
+++ app/deploy/update.sh
@@ -8,6 +8,6 @@ git pull
 $PROJECT_PATH/env/bin/python3 -m pip install -r $PROJECT_PATH/requirements.txt
 $PROJECT_PATH/env/bin/python3 manage.py migrate
 $PROJECT_PATH/env/bin/python3 manage.py collectstatic --noinput
-supervisorctl restart profiles_api
+supervisorctl restart care168_profiles_api
 
 echo "DONE! :)"
