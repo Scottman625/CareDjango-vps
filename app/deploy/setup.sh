4f5a7013450e056a4007934b020246e27450a53b
Scottman625 <scottman608@gmail.com>
2025-03-06 14:54:26 +0800

    update deploy file

--- app/deploy/setup.sh
+++ app/deploy/setup.sh
@@ -34,15 +34,15 @@ $PROJECT_BASE_PATH/env/bin/python3 manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python3 manage.py collectstatic --noinput
 
 # Configure supervisor
-cp $PROJECT_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
+cp $PROJECT_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/care168_profiles_api.conf
 supervisorctl reread
 supervisorctl update
-supervisorctl restart profiles_api
+supervisorctl restart care168_profiles_api
 
 # Configure nginx
-cp $PROJECT_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
+cp $PROJECT_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/care168_profiles_api.conf
 rm /etc/nginx/sites-enabled/default
-ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
+ln -s /etc/nginx/sites-available/care168_profiles_api.conf /etc/nginx/sites-enabled/care168_profiles_api.conf
 systemctl restart nginx.service
 
 echo "DONE! :)"
